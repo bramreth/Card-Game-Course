5bf440b2c87cec8a1baa24a0faf6c9102399ab8a
bram <bramreth@gmail.com>
2025-02-14 01:22:50 +0000

    8 - Lanes and Animations (part 1)

--- card-game-course/lane.gd
+++ card-game-course/lane.gd
@@ -15,7 +15,8 @@ func add_card(card: Node3D) -> void:
 	
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event.is_action_pressed("Click"):
-		selected.emit(self)
+		if is_empty():
+			selected.emit(self)
 
 func _on_area_3d_mouse_entered() -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
@@ -23,3 +24,6 @@ func _on_area_3d_mouse_entered() -> void:
 
 func _on_area_3d_mouse_exited() -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+
+func is_empty() -> bool:
+	return card_slot.get_child_count() == 0
